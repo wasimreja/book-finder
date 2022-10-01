211448627e9216587ab1e93336a73fd0e462c23c
Thomas <thomasmartin98@hotmail.com>
2022-10-01 19:48:10 +0100

    Add responsive grid for books plus add animations for hovering text overflow

--- main.js
+++ main.js
@@ -1,56 +1,69 @@
 function search() {
-	var search = document.getElementById('input').value;
-	document.getElementById('input').value = '';
-	console.log('Working');
-	$.ajax({
-		url: 'https://www.googleapis.com/books/v1/volumes?q=' + search + "&maxResults=20",
-		dataType: 'json',
+  var search = document.getElementById("input").value;
+  document.getElementById("input").value = "";
+  console.log("Working");
+  $.ajax({
+    url:
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+      search +
+      "&maxResults=20",
+    dataType: "json",
 
-		success: function(res) {
-			var myNode = document.getElementById("results");
-			while (myNode.firstChild) {
-				myNode.removeChild(myNode.firstChild);
-			}
-			for (var i = 0; i < res.items.length; i++) {
-				console.log(res.items[i].volumeInfo.title + " " + res.items[i].volumeInfo.subtitle + " " + res.items[i].volumeInfo.authors + " " + res.items[i].volumeInfo.imageLinks.smallThumbnail);
-        		
-				// DIV				
-				var div = document.createElement("DIV");
-				
-				// Image
-				var img = document.createElement("IMG");
-				img.src=res.items[i].volumeInfo.imageLinks.smallThumbnail;
-		
-				// Title
-				var h1 = document.createElement("H1");
-				var title = document.createTextNode(res.items[i].volumeInfo.title);
-				h1.appendChild(title);
-				
-				// Description
-				var par = document.createElement("p");
-				var desc = document.createTextNode(res.items[i].volumeInfo.description);
-				par.appendChild(desc);
-		
-				// Button
-				var btn = document.createElement("BUTTON");
-				btn.innerHTML = "Read";
-				btn.setAttribute("onclick","location.href = ' "+res.items[i].volumeInfo.previewLink+" '; ");
-		
-				btn.classList.add("btn");
-				btn.classList.add("btn-outline-secondary");
-				div.classList.add("result");
-				div.classList.add("container");
-		
-				div.appendChild(h1);
-				div.appendChild(img);
-				div.appendChild(par);
-				div.appendChild(btn);
-				document.getElementById('results').appendChild(div);
-			}
-		},
-		maxResults: 30,
-		type: 'GET'
-	});
+    success: function (res) {
+      var myNode = document.getElementById("results");
+      while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+      }
+      for (var i = 0; i < res.items.length; i++) {
+        console.log(res.items[i].volumeInfo.imageLinks);
+
+        // Card
+        var card = document.createElement("DIV");
+        card.style.backgroundImage = `url(${res.items[i].volumeInfo.imageLinks.thumbnail})`;
+        card.classList.add("result");
+
+        //Card content
+        var card_content = document.createElement("DIV");
+
+        // Title
+        var h1 = document.createElement("H1");
+        var title = document.createTextNode(res.items[i].volumeInfo.title);
+        h1.appendChild(title);
+
+        // Description
+        var par = document.createElement("p");
+        var desc = document.createTextNode(
+          res.items[i].volumeInfo.description ?? ""
+        );
+        par.appendChild(desc);
+        par.classList.add("card-desc");
+
+        // Button
+        var btn = document.createElement("BUTTON");
+        btn.innerHTML = "Read";
+        btn.setAttribute(
+          "onclick",
+          "location.href = ' " + res.items[i].volumeInfo.previewLink + " '; "
+        );
+
+        btn.classList.add("btn");
+        btn.classList.add("btn-outline-secondary");
+
+        card_content.appendChild(h1);
+        card_content.appendChild(par);
+        card_content.appendChild(btn);
+        card_content.classList.add("card-content-container");
+
+        card.appendChild(card_content);
+
+        document.getElementById("results").appendChild(card);
+      }
+    },
+    maxResults: 30,
+    type: "GET",
+  });
 }
 
-document.getElementById('button-addon1').addEventListener('click', search, false);
+document
+  .getElementById("button-addon1")
+  .addEventListener("click", search, false);
