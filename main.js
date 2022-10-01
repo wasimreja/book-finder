58e556b363b5882b13d82856a69ae446d0bf81f1
Luis Sánchez <sanchezluis9820@gmail.com>
2022-10-01 13:04:29 -0500

    fix read button

--- main.js
+++ main.js
@@ -1,57 +1,72 @@
-function search() {
-	var search = document.getElementById('input').value;
-	document.getElementById('input').value = '';
-	$.ajax({
-		url: 'https://www.googleapis.com/books/v1/volumes?q=' + search + "&maxResults=20",
-		dataType: 'json',
+function search(e) {
+  e.preventDefault();
+  var search = document.getElementById("input").value;
 
-		success: function(res) {
-			var myNode = document.getElementById("results");
-			while (myNode.firstChild) {
-				myNode.removeChild(myNode.firstChild);
-			}
-			for (var i = 0; i < res.items.length; i++) {
-				//log for check data info
-				// console.log(res.items[i].volumeInfo.title + " " + res.items[i].volumeInfo.subtitle + " " + res.items[i].volumeInfo.authors + " " + res.items[i].volumeInfo.imageLinks.smallThumbnail);
-        		
-				// DIV				
-				var div = document.createElement("DIV");
-				
-				// Image
-				var img = document.createElement("IMG");
-				img.src=res.items[i].volumeInfo.imageLinks.smallThumbnail;
-		
-				// Title
-				var h1 = document.createElement("H1");
-				var title = document.createTextNode(res.items[i].volumeInfo.title);
-				h1.appendChild(title);
-				
-				// Description
-				var par = document.createElement("p");
-				var desc = document.createTextNode(res.items[i].volumeInfo.description);
-				par.appendChild(desc);
-		
-				// Button
-				var btn = document.createElement("a");
-				btn.innerHTML = "Read";
-				btn.href = res.items[i].volumeInfo.previewLink
-				btn.target = "_blank"
+  document.getElementById("input").value = "";
+  document.activeElement.blur(); // this removes focus on the input bar after search
 
-				btn.classList.add("btn");
-				btn.classList.add("btn-outline-secondary");
-				div.classList.add("result");
-				div.classList.add("container");
-		
-				div.appendChild(h1);
-				div.appendChild(img);
-				div.appendChild(par);
-				div.appendChild(btn);
-				document.getElementById('results').appendChild(div);
-			}
-		},
-		maxResults: 30,
-		type: 'GET'
-	});
+
+  $.ajax({
+    url:
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+      search +
+      "&maxResults=20",
+    dataType: "json",
+
+    success: function (res) {
+      var myNode = document.getElementById("results");
+      while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+      }
+      for (var i = 0; i < res.items.length; i++) {
+        // console.log(
+        //   res.items[i].volumeInfo.title +
+        //     " " +
+        //     res.items[i].volumeInfo.subtitle +
+        //     " " +
+        //     res.items[i].volumeInfo.authors +
+        //     " " +
+        //     res.items[i].volumeInfo.imageLinks.smallThumbnail
+        // );
+
+        // DIV
+        var div = document.createElement("DIV");
+
+        // Image
+        var img = document.createElement("IMG");
+        img.src = res.items[i].volumeInfo.imageLinks.smallThumbnail;
+
+        // Title
+        var h1 = document.createElement("H1");
+        var title = document.createTextNode(res.items[i].volumeInfo.title);
+        h1.appendChild(title);
+
+        // Description
+        var par = document.createElement("p");
+        var desc = document.createTextNode(res.items[i].volumeInfo.description);
+        par.appendChild(desc);
+
+        // Button
+        var btn = document.createElement("a");
+        btn.innerHTML = "Read";
+		btn.href= res.items[i].volumeInfo.previewLink
+		btn.target= "blank"
+
+        btn.classList.add("btn");
+        btn.classList.add("btn-outline-secondary");
+        div.classList.add("result");
+        div.classList.add("container");
+
+        div.appendChild(h1);
+        div.appendChild(img);
+        div.appendChild(par);
+        div.appendChild(btn);
+        document.getElementById("results").appendChild(div);
+      }
+    },
+    maxResults: 30,
+    type: "GET",
+  });
 }
 
-document.getElementById('button-addon1').addEventListener('click', search, false);
+document.querySelector(".search-form").addEventListener("submit", search);
