75e65020299952e48e03a6a81e1c929d62f51210
Chirag Aggarwal <thechiragaggarwal@gmail.com>
2022-10-03 03:51:39 +0530

    Improved UI for book list

--- main.js
+++ main.js
@@ -42,10 +42,19 @@ function search(e) {
           var div = document.createElement("DIV");
 
           // Image
+          var imgDiv = document.createElement("DIV");
+          imgDiv.classList.add("col-md-2");
+          imgDiv.classList.add("offset-md-2");
+
           var img = document.createElement("IMG");
           img.src = res.items[i].volumeInfo.imageLinks.smallThumbnail;
+          img.classList.add("w-100");
+
+          imgDiv.appendChild(img);
 
           // Title
+          var textDiv = document.createElement("DIV");
+          textDiv.classList.add("col-md-8");
           var h1 = document.createElement("H1");
           var title = document.createTextNode(res.items[i].volumeInfo.title);
           h1.appendChild(title);
@@ -63,20 +72,22 @@ function search(e) {
 
           // Button
           var btn = document.createElement("a");
-          btn.innerHTML = "Read";
+          btn.innerHTML = "READ NOW";
           btn.href = res.items[i].volumeInfo.previewLink
           btn.target = "blank"
 
           btn.classList.add("btn");
           btn.classList.add("btn-outline-secondary");
           div.classList.add("result");
-          div.classList.add("container");
+          div.classList.add("row");
 
-          div.appendChild(h1);
-          div.appendChild(p);
-          div.appendChild(img);
-          div.appendChild(par);
-          div.appendChild(btn);
+          textDiv.appendChild(h1);
+          textDiv.appendChild(p);
+          textDiv.appendChild(par);
+          textDiv.appendChild(btn);
+
+          div.appendChild(textDiv);
+          div.appendChild(imgDiv);
           document.getElementById("results").appendChild(div);
         }
       }
