f100a7175e8f9e32fc1978456a88d48b224f3088
Rohan Gope <109970907+Darkrider0007@users.noreply.github.com>
2022-10-02 11:45:18 +0530

    Add a Animation

--- main.js
+++ main.js
@@ -71,4 +71,51 @@ function search(e) {
   });
 }
 
+var words = ['Hi, there','I am help you to find out books'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
+
 document.querySelector(".search-form").addEventListener("submit", search);
