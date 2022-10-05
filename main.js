05e418d8afc68836773df982e3977cc1d4028c8a
martinwangata <97232867+martinwangata@users.noreply.github.com>
2022-10-06 02:26:38 +0300

    Wangata Commits

--- main.js
+++ main.js
@@ -50,6 +50,20 @@ function search(e) {
 
           imgDiv.appendChild(img);
 
+          // NavBar
+          
+          window.onscroll = function() {myFunction()};
+          var navbar = document.getElementById("navbar");
+          var sticky = navbar.offsetTop;
+          function myFunction() {
+            if (window.pageYOffset >= sticky) {
+              navbar.classList.add("sticky")
+            } else {
+              navbar.classList.remove("sticky");
+            }
+          }
+
+
           // Title
           var textDiv = document.createElement("DIV");
           textDiv.classList.add("col-md-8");
