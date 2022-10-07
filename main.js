ad24139b911f5a591ddf37fbf2ba50b127fca3fe
disha815 <dishaarora193@gmail.com>
2022-10-07 17:38:59 +0530

    Modified index.html , style.css and main.js to add  a scroll up button when scrolling down would help to navigate to top instantly

--- main.js
+++ main.js
@@ -97,3 +97,22 @@ function search(e) {
 }
 
 document.querySelector(".search-form").addEventListener("submit", search);
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0; 
+}
