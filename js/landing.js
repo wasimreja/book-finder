c621a21de3bbfda87b5e043e2575e01bd95e5a2b
Akshat Jain <84455686+AK-shat-JAIN@users.noreply.github.com>
2023-02-24 02:55:07 +0530

    Added animated Landing Page

---
+++ js/landing.js
@@ -0,0 +1,8 @@
+// const l=document.querySelectorAll("#logo path");
+// for(let i=0;i<l.length;i++){
+//     console.log(`letter ${i} is ${l[i].getTotalLength()}`)
+// }
+function redirectpage(){
+    window.location="index.html";
+}
+setTimeout('redirectpage()', 4000);
