0c89bcd76dacd991da18c6d4ae94002288b8520b
Wasim Reja <iwasimreja@gmail.com>
2023-03-04 16:50:52 +0530

    Code refactor

---
+++ js/error.js
@@ -0,0 +1,10 @@
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    distance: '90px',
+    duration: 3000,
+})
+
+sr.reveal(`.home__data`, {origin: 'top', delay: 400})
+sr.reveal(`.home__img`, {origin: 'bottom', delay: 600})
+sr.reveal(`.home__footer`, {origin: 'bottom', delay: 800})
